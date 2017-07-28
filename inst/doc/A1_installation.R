2205cd1a9cebbf9b2a3e71bff2ef54f8d524fe0a
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2017-07-28 18:23:35 +0200

    update docu to deliverable D.4.1

--- inst/doc/A1_installation.R
+++ inst/doc/A1_installation.R
@@ -1,1 +1,9 @@
+## ----eval = FALSE--------------------------------------------------------
+#  system.file(package = "aquanes.report")
+
+## ----eval = FALSE--------------------------------------------------------
+#  system.file("shiny/haridwar", package = "aquanes.report")
+
+## ----eval = FALSE--------------------------------------------------------
+#  system.file("shiny/haridwar/data", package = "aquanes.report")
 
