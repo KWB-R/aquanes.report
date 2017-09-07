676f17f3bb4f2e91a4627f9bc910f333a94a2021
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2017-09-07 15:37:51 +0200

    berlin_s: CSV import performance improved by ~250% (base::read.csv -> data.table::fread), join with parameter_site_metadata.csv now based on "ProzessID"

--- R/import_operation_berlin_s.R
+++ R/import_operation_berlin_s.R
@@ -2,17 +2,17 @@
 #' @param raw_data_file file path to raw data which should be used for as template
 #' for meta file creation
 #' @return data.frame with meta data file structure
+#' @importFrom data.table fread
+#' @importFrom tidyr separate
 #' @export
 create_wedeco_metafile <- function(raw_data_file) {
 
 
   print(paste("Importing raw data file:",raw_data_file ))
-  ozone <- read.csv(file = raw_data_file ,
+  ozone <- data.table::fread(input = raw_data_file ,
                     header = TRUE,
                     sep = ";",
-                    dec = ",",
-                    skip = 2,
-                    stringsAsFactors = FALSE)
+                    skip = 2)
 
 
   indices_names <- seq(from = 1,by = 2,to = ncol(ozone) - 1)
@@ -24,8 +24,26 @@ create_wedeco_metafile <- function(raw_data_file) {
                           SiteName = rep(NA, length(indices_names)),
                           ParameterName_SiteName = names(ozone)[indices_names],
                           ParameterUnitOrg = names(ozone)[indices_units],
-                          ParameterUnit = names(ozone)[indices_units]
-  )
+                          ParameterUnit = names(ozone)[indices_units], 
+                          stringsAsFactors = FALSE) %>% 
+                tidyr::separate(col = "ParameterName_SiteName", 
+                                into  = c("ProzessSignal", 
+                                          "ProzessID", 
+                                          "ProzessName"), 
+                                sep = " - ",
+                                remove = FALSE) 
+  
+  meta_file$ProzessID <- as.numeric(meta_file$ProzessID)
+
+  meta_file$ParameterUnit <- sub(pattern = "_", 
+                                replacement = "/", 
+                                 x = meta_file$ParameterUnit)
+  
+  meta_file$ParameterUnit <- sub(pattern = "V.*", 
+                                 replacement = "", 
+                                 x = meta_file$ParameterUnit)
+  
+
 
   return(meta_file)
 
@@ -37,6 +55,7 @@ create_wedeco_metafile <- function(raw_data_file) {
 #' @param raw_data_dir path to raw data directory
 #' @param meta_file_path path to meta data file
 #' @importFrom lubridate parse_date_time2
+#' @importFrom stringr str_sub
 #' @export
 read_wedeco_data <- function(raw_data_dir = system.file("shiny/berlin_s/data/operation",
                                                         package = "aquanes.report"),
@@ -51,15 +70,21 @@ read_wedeco_data <- function(raw_data_dir = system.file("shiny/berlin_s/data/ope
 
   for (pathfile in files_to_import) {
     print(paste("Importing raw data file:",pathfile))
-    ozone <- read.csv(file = pathfile,
-                      header = TRUE,
-                      sep = ";",
-                      dec = ",",
-                      skip = 2,
-                      stringsAsFactors = FALSE)
+    ozone <- data.table::fread(input = pathfile ,
+                               header = TRUE,
+                               sep = ";",
+                               dec = ",",
+                               skip = 2)
+    
+    indices_names <- seq(from = 1,by = 2,to = ncol(ozone) - 1)
+    indices_units <- seq(from = 2,by = 2,to = ncol(ozone))
 
+    
+    process_ids <- stringr::str_sub(names(ozone)[indices_names], 6, 11)
+    
+    names(ozone)[indices_units] <- process_ids
 
-    ozone <- ozone[,c(1,seq(from = 2,by = 2,to = ncol(ozone)))]
+    ozone <- ozone[,c(1,indices_units),with=FALSE]
 
 
     names(ozone)[1] <- "DateTime"
@@ -68,6 +93,7 @@ read_wedeco_data <- function(raw_data_dir = system.file("shiny/berlin_s/data/ope
     ozone$DateTime <- lubridate::parse_date_time2(ozone$DateTime,
                                                  orders = "d!.m!*.y!* H!:M!:S!",
                                                  tz = "CET")
+    
 
     if (pathfile == files_to_import[1]) {
       df <- ozone
@@ -76,27 +102,35 @@ read_wedeco_data <- function(raw_data_dir = system.file("shiny/berlin_s/data/ope
     }
   }
 
-  ozone_tidy <- tidyr::gather_(data = df,
-                        key_col = "ParameterUnitOrg",
+  df_tidy <- tidyr::gather_(data = df,
+                        key_col = "ProzessID",
                         value_col = "ParameterValue",
-                        gather_cols = setdiff(names(df), "DateTime"))
+                        gather_cols = setdiff(names(df), "DateTime")) %>% 
+                dplyr::mutate_(ProzessID = "as.numeric(ProzessID)")
 
 
 
   meta_data  <- read.csv(file = meta_file_path,
-                                      stringsAsFactors = FALSE)
+                                      stringsAsFactors = FALSE) %>% 
+                dplyr::select_("ProzessID", 
+                               "ParameterCode",
+                               "ParameterName",
+                               "ParameterUnit",
+                               "SiteCode",
+                               "SiteName",
+                               "ZeroOne")
 
   meta_data$ParameterLabel <- sprintf("%s (%s)",
                              meta_data$ParameterName,
                              meta_data$ParameterUnit)
 
-  relevant_paras <- ozone_tidy$ParameterUnitOrg %in% meta_data$ParameterUnitOrg[meta_data$ZeroOne == 1]
+  relevant_paras <- df_tidy$ProzessID %in% meta_data$ProzessID[meta_data$ZeroOne == 1]
 
 
   meta_data <- meta_data %>%
                select_(.dots = "-ZeroOne")
 
-  df_tidy <- ozone_tidy[relevant_paras,] %>%
+  df_tidy <- df_tidy[relevant_paras,] %>%
     dplyr::left_join(y = meta_data) %>%
     as.data.frame()
 
