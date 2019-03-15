1f9dad843811b8a81625f21d350a5e7039658f04
mrustl <michael.rustler@kompetenz-wasser.de>
2019-03-15 08:11:37 +0100

    rerun documentation

--- inst/doc/A1_installation.R
+++
@@ -1,9 +0,0 @@
-## ----eval = FALSE--------------------------------------------------------
-#  system.file(package = "aquanes.report")
-
-## ----eval = FALSE--------------------------------------------------------
-#  system.file("shiny/haridwar", package = "aquanes.report")
-
-## ----eval = FALSE--------------------------------------------------------
-#  system.file("shiny/haridwar/data", package = "aquanes.report")
-
